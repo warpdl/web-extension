26898f7405ff06e964cc63c538b87a0a02db8ee9
celestix <i@anodite.me>
2024-08-05 01:01:07 +0530

    add basic download capturing logic

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,20 +1,53 @@
-// chrome.runtime.onInstalled.addListener(() => {
-//     chrome.windows.create({
-//       url: 'thanks.html',
-//       type: 'popup',
-//       width: 400,
-//       height: 500
-//     });
-// });
+chrome.runtime.onInstalled.addListener(async () => {
+    let i = await chrome.storage.local.get();
+    if (i.installed != null) {
+        return;
+    }
+    await chrome.storage.local.set({installed: true});
+    await chrome.tabs.create({
+      url: 'thanks.html',
+    });
+});
 
-chrome.downloads.onCreated.addListener((downloadItem) => {
-    var cookieString = '';
+chrome.downloads.onCreated.addListener((downloadItem: chrome.downloads.DownloadItem) => {
     chrome.downloads.cancel(downloadItem.id, () => {
         chrome.cookies.getAll({url: downloadItem.url}, (cookies) => {
-            console.log(cookies);
         });
     });
-    console.log(cookieString);
-    console.log(downloadItem);
 });
-  
+
+let headerStore = new Map<string, chrome.webRequest.WebRequestHeadersDetails>();
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    (details) => {
+        headerStore.set(details.url, details);
+    },
+    {urls: ['<all_urls>']},
+    []
+)
+
+chrome.webRequest.onBeforeRedirect.addListener(
+    (details) => {
+        let data = headerStore.get(details.url)!;
+        headerStore.delete(details.url);
+        headerStore.set(details.redirectUrl, data);
+    },
+    {urls: ['<all_urls>']},
+    []
+)
+
+chrome.webRequest.onErrorOccurred.addListener(
+    (details) => {
+        headerStore.delete(details.url);
+    },
+    {urls: ['<all_urls>']},
+    []
+)
+
+chrome.webRequest.onCompleted.addListener(
+    (details) => {
+        headerStore.delete(details.url);
+    },
+    {urls: ['<all_urls>']},
+    []
+)
